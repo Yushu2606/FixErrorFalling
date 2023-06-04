a1587e1fd8aaee6bf38e6109b9dec113130b79b4
星梦喵吖 <starsdream00@icloud.com>
2023-06-04 22:40:26 +0800

    初始化

---
+++ src/dllmain.cpp
@@ -0,0 +1,18 @@
+#pragma comment(lib, "../SDK/lib/bedrock_server_api.lib")
+#pragma comment(lib, "../SDK/lib/bedrock_server_var.lib")
+#pragma comment(lib, "../SDK/lib/SymDBHelper.lib")
+#pragma comment(lib, "../SDK/lib/LiteLoader.lib")
+
+#include <llapi/HookAPI.h>
+#include <llapi/mc/HeavyBlock.hpp>
+#include <llapi/mc/Block.hpp>
+#include <llapi/mc/BlockSource.hpp>
+#include <llapi/mc/BlockPos.hpp>
+#include <llapi/mc/Level.hpp>
+
+TInstanceHook(void, "?startFalling@HeavyBlock@@MEBAXAEAVBlockSource@@AEBVBlockPos@@AEBVBlock@@_N@Z",
+			  HeavyBlock, class BlockSource& a2, class BlockPos const& a3, class Block const& a4, bool a5) {
+	if (!Level::getBlock(a3.add(0, -1), &a2)->isAir())
+		return;
+	original(this, a2, a3, a4, a5);
+}
